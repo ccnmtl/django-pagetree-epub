f9ed866f18b1c4100797e931dfad646d92a033da
Anders Pearson <anders@columbia.edu>
2017-02-23 15:57:33 +0000

    need a setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+import os
+from setuptools import setup
+
+ROOT = os.path.abspath(os.path.dirname(__file__))
+
+setup(
+    name="django-pagetree-epub",
+    version="0.1.0",
+    author="Anders Pearson",
+    author_email="anders@columbia.edu",
+    url="https://github.com/ccnmtl/django-pagetree-epub",
+    description="make an epub from a pagetree site",
+    long_description=open(
+        os.path.join(ROOT, 'README.markdown')).read(),
+    install_requires=['Django>=1.8', 'nose',
+                      'django-pagetree', 'epubbuilder'],
+    scripts=[],
+    license="BSD",
+    platforms=["any"],
+    zip_safe=False,
+    package_data = {'': ['*.*']},
+    packages=['pagetreeepub'],
+    test_suite='nose.collector',
+)
